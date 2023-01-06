1be5cccde178a777b0058d3a8e3b3d90894e1348
azharanowar <azharanowar@gmail.com>
2023-01-06 08:26:47 +0600

    Admin login function with our custom design working

--- resources/views/admin/login/pages/login.blade.php
+++ resources/views/admin/login/pages/login.blade.php
@@ -17,28 +17,34 @@
                         <div class="col-lg-6">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    <h1 class="h4 text-gray-900 mb-4">Login Daily News Dashboard!</h1>
+                                    @if (session('status'))
+                                        <h4 class="my-3 font-medium text-sm text-green-600">
+                                            {{ session('status') }}
+                                        </h4>
+                                    @endif
                                 </div>
-                                <form class="user">
+                                <form action="{{ route('login') }}" method="POST" class="user">
+
+                                    @csrf
+
                                     <div class="form-group">
                                         <input type="email" class="form-control form-control-user"
-                                               id="exampleInputEmail" aria-describedby="emailHelp"
+                                               id="email" name="email" aria-describedby="Email Address"
                                                placeholder="Enter Email Address...">
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control form-control-user"
-                                               id="exampleInputPassword" placeholder="Password">
+                                               id="password" name="password" placeholder="Password">
                                     </div>
                                     <div class="form-group">
                                         <div class="custom-control custom-checkbox small">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck">
-                                            <label class="custom-control-label" for="customCheck">Remember
+                                            <input type="checkbox" class="custom-control-input" id="remember_me" name="remember">
+                                            <label class="custom-control-label" for="remember_me">Remember
                                                 Me</label>
                                         </div>
                                     </div>
-                                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                        Login
-                                    </a>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
                                     <hr>
                                     <a href="index.html" class="btn btn-google btn-user btn-block">
                                         <i class="fab fa-google fa-fw"></i> Login with Google
