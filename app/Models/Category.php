5d5465ae0f4716af406321706fc8835fc9e588b4
azharanowar <azharanowar@gmail.com>
2023-01-07 00:27:22 +0600

    Category raw data save to database completed

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,9 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-    private $category, $categories, $image, $imageNewName, $directory, $imageURL;
+    private static $category, $categories, $image, $imageNewName, $directory, $imageURL;
 
     public static function saveNewCategory($request) {
-        //
+        self::$category = new Category();
+
+        self::$category->name = $request->name;
+        self::$category->description = $request->description;
+        self::$category->image = $request->image;
+        self::$category->status = $request->status;
+        self::$category->save();
     }
 }
--- resources/views/admin/dashboard/category/add-category.blade.php
+++ resources/views/admin/dashboard/category/add-category.blade.php
@@ -11,7 +11,7 @@
 
                 <h2 class="text-center">Add New Category</h2>
                 @if(session('message'))
-                    <h4 class="text-center text-success">{{ session('message') }}</h4>
+                    <h4 class="text-center text-success py-3">{{ session('message') }}</h4>
                 @endif
 
                 <form action="{{ route('category.save-category') }}" method="POST" class="form" enctype="multipart/form-data">
@@ -19,19 +19,19 @@
                     @csrf
 
                     <div class="mb-3">
-                        <label for="name" class="form-label">Name</label>
+                        <label for="name" class="form-label">Name: </label>
                         <input type="text" class="form-control" id="name" name="name" placeholder="Enter category name..." autofocus required>
                     </div>
                     <div class="mb-3">
                         <label for="description" class="form-label">Description</label>
-                        <textarea class="form-control" id="description" name="description" rows="4" placeholder="Enter category description..." required></textarea>
+                        <textarea class="form-control" id="description" name="description" rows="4" placeholder="Enter category description..."></textarea>
                     </div>
                     <div class="mb-3">
-                        <label for="image" class="form-label">Category Image</label>
-                        <input type="file" class="form-control" id="image" style="height: auto;" name="image" required accept="image/*">
+                        <label for="image" class="form-label">Category Image: </label>
+                        <input type="file" class="form-control" id="image" style="height: auto;" name="image" accept="image/*">
                     </div>
                     <div class="mb-3">
-                        <label for="status" class="form-label">Publication Status</label>
+                        <label for="status" class="form-label">Publication Status: </label>
                         <div class="bg-white rounded border py-2 px-3">
                             <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="radio" name="status" id="activeStatus" value="1" checked>
