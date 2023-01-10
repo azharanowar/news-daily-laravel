cb10d95fc73feedf7269c7b5de7d167c991a6806
azharanowar <azharanowar@gmail.com>
2023-01-11 01:03:40 +0600

    Category archive page load all news based on category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends Controller
 
         return view('frontEnd.category.index', [
             'category'  =>  $this->category,
-            'news'      =>  News::where('category_id', $this->category->id),
+            'news'      =>  News::where('category_id', $this->category->id)->where('status', 1)->get(),
         ]);
     }
 }
--- resources/views/frontEnd/category/index.blade.php
+++ resources/views/frontEnd/category/index.blade.php
@@ -15,16 +15,16 @@
                     @foreach($news as $item)
                         <div class="d-md-flex post-entry-2 half">
                         <a href="" class="me-4 thumbnail">
-                            <img src="{{ asset($item->image) }}" alt="{{ $item->title }}" class="img-fluid">
+                            <img src="{{ asset($item->featured_image) }}" alt="{{ $item->title }}" class="img-fluid">
                         </a>
                         <div>
-                            <div class="post-meta"><span class="date">{{ $item->category->name }}</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
-                            <h3><a href="single-post.html">What is the son of Football Coach John Gruden, Deuce Gruden doing Now?</a></h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat exercitationem magni voluptates dolore. Tenetur fugiat voluptates quas, nobis error deserunt aliquam temporibus sapiente, laudantium dolorum itaque libero eos deleniti?</p>
+                            <div class="post-meta"><span class="date">{{ $item->category->name }}</span> <span class="mx-1">&bullet;</span> <span>{{ $item->created_at->format('d-M-Y') }}</span></div>
+                            <h3><a href="">{{ $item->title }}</a></h3>
+                            <p>{{ $item->short_description }}</p>
                             <div class="d-flex align-items-center author">
-                                <div class="photo"><img src="assets/img/person-2.jpg" alt="" class="img-fluid"></div>
+                                <div class="photo"><img src="{{ asset('frontEnd') }}/assets/img/person-2.jpg" alt="" class="img-fluid"></div>
                                 <div class="name">
-                                    <h3 class="m-0 p-0">Wade Warren</h3>
+                                    <h3 class="m-0 p-0">{{ $item->author->name }}</h3>
                                 </div>
                             </div>
                         </div>
